ce928c7d52d2917b2c47e0682bef306e02fa2aa4
Eugene <93196991+chernyaeugene@users.noreply.github.com>
2022-08-24 19:08:38 +0300

    adaptive

--- Js.js
+++ Js.js
@@ -55,10 +55,21 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
     loop : true,
-    autoplay: {
-        delay: 2500,
-        stopOnLastSlide: false,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 2500,
+    //     stopOnLastSlide: false,
+    //     disableOnInteraction: false,
+    // },
     speed: 2500,
 });
+
+// бургер
+const iconMenu = document.querySelector('.menu_icon');
+const menuBody = document.querySelector('.menu_box_list');
+if (iconMenu){
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
