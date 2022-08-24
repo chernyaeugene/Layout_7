025da1fa09508fd798b32f7e998eea0f9fdcdea1
Eugene <93196991+chernyaeugene@users.noreply.github.com>
2022-08-25 00:10:59 +0300

    adaptive finish

--- Js.js
+++ Js.js
@@ -55,11 +55,11 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
     loop : true,
-    // autoplay: {
-    //     delay: 2500,
-    //     stopOnLastSlide: false,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        stopOnLastSlide: false,
+        disableOnInteraction: false,
+    },
     speed: 2500,
 });
 
