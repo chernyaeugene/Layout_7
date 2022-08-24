edc478e10daffc06a8c6a10a5b59ca618342f14f
Eugene <93196991+chernyaeugene@users.noreply.github.com>
2022-08-24 13:09:24 +0300

    adaptive

--- Js.js
+++ Js.js
@@ -42,18 +42,14 @@ if (isMobile.any()) {
 } else {
     document.body.classList.add('_pc');
 }
-
+// Слайдер
 const swiper = new Swiper('.swiper', {
     spaceBetween: 30,
     parallax: true,
-
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -65,5 +61,4 @@ const swiper = new Swiper('.swiper', {
         disableOnInteraction: false,
     },
     speed: 2500,
-
 });
